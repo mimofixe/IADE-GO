4e3253a61a960f59f146c853ac5ecbc0f94dbd92
David Bação <136340847+davidbacao03@users.noreply.github.com>
2025-11-08 18:11:12 +0000

    Add files via upload

---
+++ server/src/main/java/com/iade/iadego/IadegoApplication.java
@@ -0,0 +1,13 @@
+package com.iade.iadego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IadegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IadegoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/iade/iadego/IadegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iade.iadego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IadegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
